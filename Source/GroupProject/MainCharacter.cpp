5f6bcc43c627b736ec608cbcab10f61fcd29c92e
pmHaugen <60093440+pmHaugen@users.noreply.github.com>
2020-03-03 15:50:48 +0100

    Update MainCharacter.cpp

--- Source/GroupProject/MainCharacter.cpp
+++ Source/GroupProject/MainCharacter.cpp
@@ -16,10 +16,10 @@ AMainCharacter::AMainCharacter()
 
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 800.f;
+	CameraBoom->TargetArmLength = 1500.f;
 	CameraBoom->bEnableCameraLag = true;
 	CameraBoom->CameraLagSpeed = 40.f;
-	CameraBoom->SetRelativeRotation(FRotator(0.f, 0.f, 0.f));
+	CameraBoom->SetRelativeRotation(FRotator(0.f, -30.f, 0.f));
 	CameraBoom->bUsePawnControlRotation = false;
 	CameraBoom->bInheritYaw = false;
 	CameraBoom->bDoCollisionTest = false;
@@ -27,7 +27,7 @@ AMainCharacter::AMainCharacter()
 	//Make the camera follow
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Follow Camera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
-	FollowCamera->SetRelativeRotation(FRotator(0.f, 0.f, 0.f));
+	FollowCamera->SetRelativeRotation(FRotator(0.f, 0.f, 400.f));
 
 }
 
