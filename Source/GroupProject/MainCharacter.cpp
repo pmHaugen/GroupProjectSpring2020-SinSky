af9bf6a23f572eac7b67f0a06b5befb727111b49
pmHaugen <60093440+pmHaugen@users.noreply.github.com>
2020-03-03 13:52:23 +0100

    Update to character cpp

--- Source/GroupProject/MainCharacter.cpp
+++ Source/GroupProject/MainCharacter.cpp
@@ -2,12 +2,31 @@
 
 
 #include "MainCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 AMainCharacter::AMainCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 800.f;
+	CameraBoom->bEnableCameraLag = true;
+	CameraBoom->SetRelativeRotation(FRotator(0.f, -30.f, 50.f));
+	CameraBoom->bUsePawnControlRotation = false;
+	CameraBoom->bInheritYaw = false;
+	CameraBoom->bDoCollisionTest = false;
+
+	//Make the camera follow
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Follow Camera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->SetRelativeRotation(FRotator(0.f, 15.f, 0.f));
 
 }
 
--- Source/GroupProject/MainCharacter.h
+++ Source/GroupProject/MainCharacter.h
@@ -15,6 +15,15 @@ public:
 	// Sets default values for this character's properties
 	AMainCharacter();
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	class UCameraComponent* FollowCamera;
+
+	float WalkSpeed;
+	float RunSpeed;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -22,6 +31,16 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	//UFUNCTION()
+		//void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+			//UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex,
+			//bool bFromSweep, const FHitResult& SweepResult);
+
+	//void MoveSideways(float Value);
+	//void MoveForward(float Value);
+
+
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
