a66d6afdda5a95ba3f237bf2b66aed04b54fcd10
Andreas <andlodding@gmail.com>
2020-05-29 15:39:10 +0200

    Cleaned Headache.h

--- Source/GroupProject/LevelTrigger.h
+++ Source/GroupProject/LevelTrigger.h
@@ -25,25 +25,6 @@ public:
 	
 	FName NextLevel;
 
-	//Selection of levels can be done in blueprints.
-
-	/**UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "LevelSelect")
-	TAssetPtr<UWorld> Level_1;
-
-	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "LevelSelect")
-	TAssetPtr<UWorld> Level_2;
-
-	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "LevelSelect")
-	TAssetPtr<UWorld> Level_3;
-
-	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "LevelSelect")
-	TAssetPtr<UWorld> Level_4;
-
-	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "LevelSelect")
-	TAssetPtr<UWorld> Level_5;
-
-	TArray<TAssetPtr<UWorld>> LevelArray;*/
-
 	void LevelSelection();
 
 	bool bCanSwitchLevel;
@@ -58,8 +39,4 @@ public:
 
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-	/**UFUNCTION(BlueprintPure, Category = "Spawning")
-	TAssetPtr<UWorld> GetLevel();*/
-
 };
