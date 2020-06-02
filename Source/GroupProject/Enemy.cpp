e62e869fb4e50d6692eec359e89269e4f3d99c2f
Andreas <andlodding@gmail.com>
2020-06-03 00:55:58 +0200

    Removed functionality responsible for crashes

--- Source/GroupProject/Enemy.cpp
+++ Source/GroupProject/Enemy.cpp
@@ -336,14 +336,6 @@ void AEnemy::MoveToTarget(AMainCharacter* Target)
 			AIController->StopMovement();
 			SetEnemyMovementStatus(EEnemyMovementStatus::EMS_Idle);
 		}
-
-		/*auto PathPoints = NavPath->GetPathPoints();
-		for (auto Point : PathPoints)
-		{
-			FVector Location = Point.Location;
-
-			//UKismetSystemLibrary::DrawDebugSphere(this, Location, 25.f, 8, FLinearColor::Red, 10.f, 1.5f);
-		}*/
 	}
 }
 
