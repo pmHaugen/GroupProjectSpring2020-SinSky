0af74767d7665fc0bc95ea717915cf97344b379a
Andreas <andlodding@gmail.com>
2020-06-02 19:50:37 +0200

    Movement, importing, deleting and creating

---
+++ Source/GroupProject/BossAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BossAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GROUPPROJECT_API UBossAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
